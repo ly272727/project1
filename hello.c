33da502a1fb22dc3b8a50d90d21eea7e6970f012
ly232323 <ly727212@163.com>
2022-01-19 20:56:06 +0800

    a project

---
+++ hello.c
@@ -0,0 +1,13 @@
+include<iostream>
+<<<<<<< HEAD
+using namespace std;
+void main(){
+cout<<"hello world.........."<<endl;
+cout<<"1"<<endl;
+=======
+using namespace std
+void main(){
+cout<<"hello"<<endl;
+print（"system"）;
+>>>>>>> main
+}
